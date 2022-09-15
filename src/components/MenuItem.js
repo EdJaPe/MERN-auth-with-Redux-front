d3b27829618ff5b330f4b09ef12a5f5451fd464b
EdJaPe <ed.stu.ga.1214@gmail.com>
2022-09-15 18:20:26 -0500

    got working fetch working

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,13 +3,46 @@ import axios from "axios";
 
 
 const MenuItem = () => {
-  const [ menuItem, setMenuItem ] = useState('')
-  const [menu, setMenu] = useState([])
+  const [ menuItem, setMenuItem ] = useState({
+    category_name: '',
+    sub_cat: '',
+    description: '',
+    price: ''
+  })
+  const [ fetchedMenu, setFetchedMenu ] = useState([])
+  const { category_name, sub_cat, description, price } = menuItem;
+  
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const config = {
+        header: {
+          "Content-Type": "application/json"
+        }
+      }
+      try{
+        const request = await axios.get(`${process.env.REACT_APP_SERVER_URL}/menu/all`, config)
+        // console.log(request.data.menu)
+        setFetchedMenu(request.data.menu)
+      } catch(error){
+        console.log(error, "error here")
+      }
+      // return request;
+    }
+    fetchMenu();
+  },[])
 
-  const menuItemHandler = (e) => {
-    setMenuItem(e.currentTarget.value);
-  }
-
+  const menuView = fetchedMenu.length < 0 ? <h3>Hold on tight..</h3>: fetchedMenu.map( (item, index) => {
+    return(
+      <div key={index} >
+        <h4>{index}</h4>
+        <h2>{item.category_name}</h2>
+        <h3>{item.sub_cat}</h3>
+        <p>{item.description}</p>               
+        <h3>{item.price}</h3>
+      </div>
+    )
+  } )
+  
   const submitHandler = (e) => {
     const config = {
       header:{
@@ -30,8 +63,18 @@ const MenuItem = () => {
 
   return(
     <div className="flex flex-col">
-      {menuItem}
-      <input onChange={menuItemHandler} type='text'/>
+      {menuView}
+      <label htmlFor="categoryName"  >Name of Category</label>
+      <input 
+      onChange={e => setMenuItem({ ...menuItem, category_name : e.target.value})} 
+      type='text' 
+      name="categoryName" required/>
+      <label htmlFor="subCat"  >Name of Sub Category</label>
+      <input type='text'  name='subCat' onChange={e => setMenuItem({ ...menuItem, sub_cat : e.target.value})} required/>
+      <label htmlFor="description"  >Description</label>
+      <input type='text'  onChange={e => setMenuItem({ ...menuItem, description: e.target.value})} name='description' required/>
+      <label htmlFor="price"  > Price </label>
+      <input type='number'  onChange={e => setMenuItem({ ...menuItem, price : e.target.value})} name='price' required /> 
       <button onClick={submitHandler} className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Add Item</button>
     </div>
   )
